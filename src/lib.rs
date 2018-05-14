1faea72f919646e1bb024ccdf2440f5fbf531a69
Hanno Braun <hanno@braun-robotics.com>
2018-05-14 08:19:02 +0200

    feat: Remove macro re-exports
    They are just a convenience that doesn't justify the use of an unstable
    feature. Also, most of them are gone in the latest lpc82x version, for
    the same reason.

--- src/lib.rs
+++ src/lib.rs
@@ -226,7 +226,6 @@
 #![feature(const_fn)]
 #![feature(const_refcell_new)]
 #![feature(never_type)]
-#![cfg_attr(feature = "rt", feature(use_extern_macros))]
 
 #![deny(warnings)]
 #![deny(missing_docs)]
@@ -263,13 +262,6 @@ pub use raw::{
     SCB,
     SYST,
     Interrupt,
-};
-
-#[cfg(feature = "rt")]
-pub use raw::{
-    default_handler,
-    exception,
-    interrupt,
 };
 
 pub use self::gpio::GPIO;
