1591ceeb9c249b6395206cc504b36ae2e8151d11
Hanno Braun <hanno@braun-robotics.com>
2018-05-18 06:47:19 +0200

    refactor: Remove unused macro argument

--- src/swm.rs
+++ src/swm.rs
@@ -264,7 +264,6 @@ macro_rules! movable_functions {
         $(
             $field:ident,
             $type:ident,
-            $reg_type:ident,
             $reg_name:ident,
             $reg_field:ident;
         )*
@@ -319,54 +318,54 @@ macro_rules! movable_functions {
 }
 
 movable_functions!(
-    u0_txd       , U0_TXD       , PINASSIGN0 , pinassign0 , u0_txd_o;
-    u0_rxd       , U0_RXD       , PINASSIGN0 , pinassign0 , u0_rxd_i;
-    u0_rts       , U0_RTS       , PINASSIGN0 , pinassign0 , u0_rts_o;
-    u0_cts       , U0_CTS       , PINASSIGN0 , pinassign0 , u0_cts_i;
-    u0_sclk      , U0_SCLK      , PINASSIGN1 , pinassign1 , u0_sclk_io;
-    u1_txd       , U1_TXD       , PINASSIGN1 , pinassign1 , u1_txd_o;
-    u1_rxd       , U1_RXD       , PINASSIGN1 , pinassign1 , u1_rxd_i;
-    u1_rts       , U1_RTS       , PINASSIGN1 , pinassign1 , u1_rts_o;
-    u1_cts       , U1_CTS       , PINASSIGN2 , pinassign2 , u1_cts_i;
-    u1_sclk      , U1_SCLK      , PINASSIGN2 , pinassign2 , u1_sclk_io;
-    u2_txd       , U2_TXD       , PINASSIGN2 , pinassign2 , u2_txd_o;
-    u2_rxd       , U2_RXD       , PINASSIGN2 , pinassign2 , u2_rxd_i;
-    u2_rts       , U2_RTS       , PINASSIGN3 , pinassign3 , u2_rts_o;
-    u2_cts       , U2_CTS       , PINASSIGN3 , pinassign3 , u2_cts_i;
-    u2_sclk      , U2_SCLK      , PINASSIGN3 , pinassign3 , u2_sclk_io;
-    spi0_sck     , SPI0_SCK     , PINASSIGN3 , pinassign3 , spi0_sck_io;
-    spi0_mosi    , SPI0_MOSI    , PINASSIGN4 , pinassign4 , spi0_mosi_io;
-    spi0_miso    , SPI0_MISO    , PINASSIGN4 , pinassign4 , spi0_miso_io;
-    spi0_ssel0   , SPI0_SSEL0   , PINASSIGN4 , pinassign4 , spi0_ssel0_io;
-    spi0_ssel1   , SPI0_SSEL1   , PINASSIGN4 , pinassign4 , spi0_ssel1_io;
-    spi0_ssel2   , SPI0_SSEL2   , PINASSIGN5 , pinassign5 , spi0_ssel2_io;
-    spi0_ssel3   , SPI0_SSEL3   , PINASSIGN5 , pinassign5 , spi0_ssel3_io;
-    spi1_sck     , SPI1_SCK     , PINASSIGN5 , pinassign5 , spi1_sck_io;
-    spi1_mosi    , SPI1_MOSI    , PINASSIGN5 , pinassign5 , spi1_mosi_io;
-    spi1_miso    , SPI1_MISO    , PINASSIGN6 , pinassign6 , spi1_miso_io;
-    spi1_ssel0   , SPI1_SSEL0   , PINASSIGN6 , pinassign6 , spi1_ssel0_io;
-    spi1_ssel1   , SPI1_SSEL1   , PINASSIGN6 , pinassign6 , spi1_ssel1_io;
-    sct_pin0     , SCT_PIN0     , PINASSIGN6 , pinassign6 , sct_in0_i;
-    sct_pin1     , SCT_PIN1     , PINASSIGN7 , pinassign7 , sct_in1_i;
-    sct_pin2     , SCT_PIN2     , PINASSIGN7 , pinassign7 , sct_in2_i;
-    sct_pin3     , SCT_PIN3     , PINASSIGN7 , pinassign7 , sct_in3_i;
-    sct_out0     , SCT_OUT0     , PINASSIGN7 , pinassign7 , sct_out0_o;
-    sct_out1     , SCT_OUT1     , PINASSIGN8 , pinassign8 , sct_out1_o;
-    sct_out2     , SCT_OUT2     , PINASSIGN8 , pinassign8 , sct_out2_o;
-    sct_out3     , SCT_OUT3     , PINASSIGN8 , pinassign8 , sct_out3_o;
-    sct_out4     , SCT_OUT4     , PINASSIGN8 , pinassign8 , sct_out4_o;
-    sct_out5     , SCT_OUT5     , PINASSIGN9 , pinassign9 , sct_out5_o;
-    i2c1_sda     , I2C1_SDA     , PINASSIGN9 , pinassign9 , i2c1_sda_io;
-    i2c1_scl     , I2C1_SCL     , PINASSIGN9 , pinassign9 , i2c1_scl_io;
-    i2c2_sda     , I2C2_SDA     , PINASSIGN9 , pinassign9 , i2c2_sda_io;
-    i2c2_scl     , I2C2_SCL     , PINASSIGN10, pinassign10, i2c2_scl_io;
-    i2c3_sda     , I2C3_SDA     , PINASSIGN10, pinassign10, i2c3_sda_io;
-    i2c3_scl     , I2C3_SCL     , PINASSIGN10, pinassign10, i2c3_scl_io;
-    adc_pintrig0 , ADC_PINTRIG0 , PINASSIGN10, pinassign10, adc_pintrig0_i;
-    acd_pintrig1 , ADC_PINTRIG1 , PINASSIGN11, pinassign11, adc_pintrig1_i;
-    acmp_o       , ACMP_O       , PINASSIGN11, pinassign11, acmp_o_o;
-    clkout       , CLKOUT       , PINASSIGN11, pinassign11, clkout_o;
-    gpio_int_bmat, GPIO_INT_BMAT, PINASSIGN11, pinassign11, gpio_int_bmat_o;
+    u0_txd       , U0_TXD       , pinassign0 , u0_txd_o;
+    u0_rxd       , U0_RXD       , pinassign0 , u0_rxd_i;
+    u0_rts       , U0_RTS       , pinassign0 , u0_rts_o;
+    u0_cts       , U0_CTS       , pinassign0 , u0_cts_i;
+    u0_sclk      , U0_SCLK      , pinassign1 , u0_sclk_io;
+    u1_txd       , U1_TXD       , pinassign1 , u1_txd_o;
+    u1_rxd       , U1_RXD       , pinassign1 , u1_rxd_i;
+    u1_rts       , U1_RTS       , pinassign1 , u1_rts_o;
+    u1_cts       , U1_CTS       , pinassign2 , u1_cts_i;
+    u1_sclk      , U1_SCLK      , pinassign2 , u1_sclk_io;
+    u2_txd       , U2_TXD       , pinassign2 , u2_txd_o;
+    u2_rxd       , U2_RXD       , pinassign2 , u2_rxd_i;
+    u2_rts       , U2_RTS       , pinassign3 , u2_rts_o;
+    u2_cts       , U2_CTS       , pinassign3 , u2_cts_i;
+    u2_sclk      , U2_SCLK      , pinassign3 , u2_sclk_io;
+    spi0_sck     , SPI0_SCK     , pinassign3 , spi0_sck_io;
+    spi0_mosi    , SPI0_MOSI    , pinassign4 , spi0_mosi_io;
+    spi0_miso    , SPI0_MISO    , pinassign4 , spi0_miso_io;
+    spi0_ssel0   , SPI0_SSEL0   , pinassign4 , spi0_ssel0_io;
+    spi0_ssel1   , SPI0_SSEL1   , pinassign4 , spi0_ssel1_io;
+    spi0_ssel2   , SPI0_SSEL2   , pinassign5 , spi0_ssel2_io;
+    spi0_ssel3   , SPI0_SSEL3   , pinassign5 , spi0_ssel3_io;
+    spi1_sck     , SPI1_SCK     , pinassign5 , spi1_sck_io;
+    spi1_mosi    , SPI1_MOSI    , pinassign5 , spi1_mosi_io;
+    spi1_miso    , SPI1_MISO    , pinassign6 , spi1_miso_io;
+    spi1_ssel0   , SPI1_SSEL0   , pinassign6 , spi1_ssel0_io;
+    spi1_ssel1   , SPI1_SSEL1   , pinassign6 , spi1_ssel1_io;
+    sct_pin0     , SCT_PIN0     , pinassign6 , sct_in0_i;
+    sct_pin1     , SCT_PIN1     , pinassign7 , sct_in1_i;
+    sct_pin2     , SCT_PIN2     , pinassign7 , sct_in2_i;
+    sct_pin3     , SCT_PIN3     , pinassign7 , sct_in3_i;
+    sct_out0     , SCT_OUT0     , pinassign7 , sct_out0_o;
+    sct_out1     , SCT_OUT1     , pinassign8 , sct_out1_o;
+    sct_out2     , SCT_OUT2     , pinassign8 , sct_out2_o;
+    sct_out3     , SCT_OUT3     , pinassign8 , sct_out3_o;
+    sct_out4     , SCT_OUT4     , pinassign8 , sct_out4_o;
+    sct_out5     , SCT_OUT5     , pinassign9 , sct_out5_o;
+    i2c1_sda     , I2C1_SDA     , pinassign9 , i2c1_sda_io;
+    i2c1_scl     , I2C1_SCL     , pinassign9 , i2c1_scl_io;
+    i2c2_sda     , I2C2_SDA     , pinassign9 , i2c2_sda_io;
+    i2c2_scl     , I2C2_SCL     , pinassign10, i2c2_scl_io;
+    i2c3_sda     , I2C3_SDA     , pinassign10, i2c3_sda_io;
+    i2c3_scl     , I2C3_SCL     , pinassign10, i2c3_scl_io;
+    adc_pintrig0 , ADC_PINTRIG0 , pinassign10, adc_pintrig0_i;
+    acd_pintrig1 , ADC_PINTRIG1 , pinassign11, adc_pintrig1_i;
+    acmp_o       , ACMP_O       , pinassign11, acmp_o_o;
+    clkout       , CLKOUT       , pinassign11, clkout_o;
+    gpio_int_bmat, GPIO_INT_BMAT, pinassign11, gpio_int_bmat_o;
 );
 
 
