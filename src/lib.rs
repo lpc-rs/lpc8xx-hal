f87eaf8d9f132e338b948550bfa6cf693fcc48e8
Hanno Braun <hanno@braun-robotics.com>
2018-06-12 09:10:38 +0200

    docs: Update `Peripherals` documentation

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,8 @@
 //! extern crate lpc82x_hal;
 //! ```
 //!
-//! That's it! Now you can start using the LPC82x HAL APIs.
+//! That's it! Now you can start using the LPC82x HAL APIs. Take a look at
+//! [`Peripherals`], which is the entry point to the whole API.
 //!
 //! Please note that LPC82x HAL is an implementation of [embedded-hal]. If your
 //! library is not specific to LPC82x, please consider depending on embedded-hal
@@ -240,10 +241,39 @@ pub mod init_state {
 
 /// Provides access to all peripherals
 ///
-/// All peripheral states are set to their default states after hardware reset.
-/// See user manual, section 5.6.14.
+/// This is the entry point to the HAL API. Before you can do anything else, you
+/// need to get an instance of this struct via [`Peripherals::take`] or
+/// [`Peripherals::steal`].
+///
+/// The HAL API tracks the state of peripherals at compile-time, to prevent
+/// potential bugs before the program can even run. Many parts of this
+/// documentation call this "type state". The peripherals available in this
+/// struct are set to their initial state (i.e. their state after a system
+/// reset). See user manual, section 5.6.14.
+///
+/// # Safe Use of the API
+///
+/// Since it should be impossible (outside of unsafe code) to access the
+/// peripherals before this struct is initialized, you can rely on the
+/// peripheral states being correct, as long as there's no bug in the API, and
+/// you're not using unsafe code to do anything that the HAL API can't account
+/// for.
+///
+/// If you directly use unsafe code to access peripherals or manipulate this
+/// API, this will be really obvious from the code. But please note that if
+/// you're using other APIs to access the hardware, such conflicting hardware
+/// access might not be obvious, as the other API might use unsafe code under
+/// the hood to access the hardware (just like this API does).
+///
+/// If you do access the peripherals in any way not intended by this API, please
+/// make sure you know what you're doing. In specific terms, this means you
+/// should be fully aware of what your code does, and whether that is a valid
+/// use of the hardware.
 pub struct Peripherals {
     /// General-purpose I/O (GPIO)
+    ///
+    /// The GPIO peripheral is enabled by default. See user manual, section
+    /// 5.6.14.
     pub gpio: GPIO<init_state::Enabled>,
 
     /// I2C0-bus interface
@@ -262,74 +292,165 @@ pub struct Peripherals {
     pub usart0: USART<raw::USART0, init_state::Disabled>,
 
     /// USART1
-    ///
-    /// The USART1 peripheral is disabled by default. See user manual, section
-    /// 5.6.14.
     pub usart1: USART<raw::USART1, init_state::Disabled>,
 
     /// USART2
-    ///
-    /// The USART2 peripheral is disabled by default. See user manual, section
-    /// 5.6.14.
     pub usart2: USART<raw::USART2, init_state::Disabled>,
 
-    /// Self-wake-up timer
+    /// Self-wake-up timer (WKT)
     pub wkt: WKT<init_state::Disabled>,
 
-    /// Analog-to-Digital Converter
+
+    /// Analog-to-Digital Converter (ADC)
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub adc: raw::ADC,
 
     /// Analog comparator
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub cmp: raw::CMP,
 
     /// CRC engine
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub crc: raw::CRC,
 
     /// DMA controller
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub dma: raw::DMA,
 
     /// DMA trigger mux
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub dmatrigmux: raw::DMATRIGMUX,
 
     /// Flash controller
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub flashctrl: raw::FLASHCTRL,
 
     /// I2C0-bus interface
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub i2c1: raw::I2C1,
 
     /// I2C0-bus interface
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub i2c2: raw::I2C2,
 
     /// I2C0-bus interface
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub i2c3: raw::I2C3,
 
     /// Input multiplexing
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub inputmux: raw::INPUTMUX,
 
     /// I/O configuration
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub iocon: raw::IOCON,
 
-    /// Multi-Rate Timer
+    /// Multi-Rate Timer (MRT)
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub mrt: raw::MRT,
 
     /// Pin interrupt and pattern match engine
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub pin_int: raw::PIN_INT,
 
-    /// State Configurable Timer
+    /// State Configurable Timer (SCT)
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub sct: raw::SCT,
 
     /// SPI0
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub spi0: raw::SPI0,
 
     /// SPI1
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub spi1: raw::SPI1,
 
-    /// Windowed Watchdog Timer
+    /// Windowed Watchdog Timer (WWDT)
+    ///
+    /// A HAL API for this peripheral has not been implemented yet. In the
+    /// meantime, this field provides you with the raw register mappings, which
+    /// allow you full, unprotected access to the peripheral.
     pub wwdt: raw::WWDT,
 }
 
 impl Peripherals {
-    /// Take the peripherals
+    /// Take the peripherals safely
+    ///
+    /// This method can only be called one time to access the peripherals. It
+    /// will return `Some(Peripherals)` when called for the first time, then
+    /// `None` on any subsequent calls.
+    ///
+    /// Applications should call this method once, at the beginning of their
+    /// main method, to get access to the full API. Any other parts of the
+    /// program should just expect to be passed whatever parts of the HAL API
+    /// they need.
+    ///
+    /// Calling this method from a library is considered an anti-pattern.
+    /// Libraries should just require whatever they need to be passed as
+    /// arguments and leave the initialization to the application that calls
+    /// them.
+    ///
+    /// For an alternative way to gain access to the hardware, please take a
+    /// look at [`Peripherals::steal`].
+    ///
+    /// # Example
+    ///
+    /// ``` no_run
+    /// use lpc82x_hal::Peripherals;
+    ///
+    /// // This code should be at the beginning of your program. As long as this
+    /// // is the only place that calls `take`, the following should never
+    /// // panic.
+    /// let p = Peripherals::take().unwrap();
+    /// ```
     pub fn take() -> Option<Self> {
         let p = raw::Peripherals::take()?;
 
@@ -337,6 +458,45 @@ impl Peripherals {
     }
 
     /// Steal the peripherals
+    ///
+    /// This function returns an instance of `Peripherals`, whether or not such
+    /// an instance exists somewhere else. This is highly unsafe, as it can lead
+    /// to conflicting access of the hardware, mismatch between actual hardware
+    /// state and peripheral state as tracked by this API at compile-time, and
+    /// in general a full nullification of all safety guarantees that this API
+    /// would normally make.
+    ///
+    /// If at all possible, you should always prefer `Peripherals::take` to this
+    /// method. The only legitimate use of this API is code that can't access
+    /// `Peripherals` the usual way, like a panic handler, or maybe temporary
+    /// debug code in an interrupt handler.
+    ///
+    /// # Safety
+    ///
+    /// This method returns an instance of `Peripherals` that might conflict
+    /// with either other instances of `Peripherals` that exist in the program,
+    /// or other means of accessing the hardware. This is only sure, if you make
+    /// sure of the following:
+    /// 1. No other code can access the hardware at the same time.
+    /// 2. You don't change the hardware state in any way that could invalidate
+    ///    the type state of other `Peripherals` instances.
+    /// 3. The type state in your `Peripherals` instance matches the actual
+    ///    state of the hardware.
+    ///
+    /// Items 1. and 2. are really tricky, so it is recommended to avoid any
+    /// situations where they apply, and restrict the use of this method to
+    /// situations where the program has effectively ended and the hardware will
+    /// be reset right after (like a panic handler).
+    ///
+    /// Item 3. applies to all uses of this method, and is generally very tricky
+    /// to get right. The best way to achieve that is probably to force the API
+    /// into a type state that allows you to execute operations that are known
+    /// to put the hardware in a safe state. Like forcing the type state for a
+    /// peripheral API to the "disabled" state, then enabling it, to make sure
+    /// it is enabled, regardless of wheter it was enabled before.
+    ///
+    /// Since there are no means within this API to forcibly change type state,
+    /// you will need to resort to something like [`core::mem::transmute`].
     pub unsafe fn steal() -> Self {
         Self::new(raw::Peripherals::steal())
     }
