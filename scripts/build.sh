1daf71418f4d3dd57bd92da66178542dcab2c1a7
Hanno Braun <hanno@braun-robotics.com>
2019-02-26 14:46:33 +0100

    Adapt build script to new configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# `.cargo/config` defaults us to the microcontroller's target triple. We need
+# to override this here, to run `cargo test`. You may need to adapt this,
+# depending on you platform.
+TARGET=x86_64-unknown-linux-gnu
+
 # Need to clean to work around this issue:
 # https://github.com/braun-robotics/rust-lpc82x-hal/issues/105
 cargo clean &&
-cargo test --verbose --features="compiletest" &&
-cargo build --verbose --features="rt" --examples --target=thumbv6m-none-eabi --release
+cargo test --verbose --features="compiletest" --target=$TARGET &&
+cargo build --verbose --features="rt" --examples --release
