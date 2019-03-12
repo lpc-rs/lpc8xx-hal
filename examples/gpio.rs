d8ab6283d7f683f4a26c01efa774a9908fc5c71c
Matthias Endler <matthias-endler@gmx.net>
2019-03-12 21:20:30 +0100

    Fix typos

--- examples/gpio.rs
+++ examples/gpio.rs
@@ -19,11 +19,11 @@ fn main() -> ! {
     // Get access to the device's peripherals. Since only one instance of this
     // struct can exist, the call to `take` returns an `Option<Peripherals>`.
     // If we tried to call the method a second time, it would return `None`, but
-    // we're only calling it the one time here, to we can call safely `unwrap`
-    // the `Option` without causing a panic.
+    // we're only calling it the one time here, so we can safely `unwrap` the
+    // `Option` without causing a panic.
     let p = Peripherals::take().unwrap();
 
-    // Initialize the APIs for of the peripherals we need.
+    // Initialize the APIs of the peripherals we need.
     let     swm    = p.SWM.split();
     let mut syscon = p.SYSCON.split();
     let mut wkt    = p.WKT.enable(&mut syscon.handle);
