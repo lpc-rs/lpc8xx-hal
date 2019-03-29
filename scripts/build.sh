df4e01548efc0844567842010922c1e01a3d2877
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-03-29 18:19:12 +0100

    Remove flags from ci script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Fail build, if there are any warnings.
-export RUSTFLAGS="-D warnings"
-
 # `.cargo/config` defaults us to the microcontroller's target triple. We need
 # to override this here, to run `cargo test`. You may need to adapt this,
 # depending on you platform.
