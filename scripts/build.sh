78cd825514f8624f22fd1f94d1b476b79c87a6bc
Hanno Braun <hanno@braun-embedded.com>
2019-10-12 11:37:39 +0200

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -e
 # Fail build, if there are any warnings.
 export RUSTFLAGS="-D warnings"
 
-cargo build --verbose --features=rt,82x --examples
-cargo build --verbose --features=rt,845 --examples
+cargo build --verbose --no-default-features --features=rt,82x --examples
+cargo build --verbose --no-default-features --features=rt,845 --examples
