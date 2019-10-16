6638553eb03d42883a95ba56426aff7271267503
Hanno Braun <hanno@braun-embedded.com>
2019-10-16 10:10:34 +0200

    Print build script error message in bold red

--- build.rs
+++ build.rs
@@ -1,10 +1,22 @@
+use termion::{
+    color,
+    style,
+};
+
+
 fn main() {
     match (cfg!(feature = "82x"), cfg!(feature = "845")) {
         (true, false) => (),
         (false, true) => (),
 
         _ => {
-            panic!("\n\n\nYou must select exactly one target platform. Pass `--features=82x` or `--features=845`\n\n\n");
+            panic!(
+                "\n\n\n{}{}You must select exactly one target platform. Pass `--features=82x` or `--features=845`{}{}\n\n\n",
+                style::Bold,
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+                style::Reset,
+            );
         }
     }
 }
