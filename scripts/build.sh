2359d970b2fa46de164d1c6da4e01884f98fe613
Hanno Braun <hanno@braun-embedded.com>
2019-10-22 08:45:56 +0200

    Simplify runtime feature selection

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -e
 # Fail build, if there are any warnings.
 export RUSTFLAGS="-D warnings"
 
-cargo build --verbose --no-default-features --features=82x-rt,82x --examples
-cargo build --verbose --no-default-features --features=845-rt,845 --examples
+cargo build --verbose --no-default-features --features=82x-rt --examples
+cargo build --verbose --no-default-features --features=845-rt --examples
