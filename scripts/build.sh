4e0491ee9ac8e386c6390a268cd772a8dc15220e
David Sawatzke <d-git@sawatzke.dev>
2020-01-08 11:05:46 +0100

    Move rustfmt to build.sh & only execute it on stable

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,11 @@ set -e
 
 # Fail build, if there are any warnings.
 export RUSTFLAGS="-D warnings"
+#
+# Check for formatting with the stable rustfmt
+if [ "$TRAVIS_RUST_VERSION" != beta ] &&  [ "$TRAVIS_RUST_VERSION" != nightly ]; then
+    cargo fmt -- --check
+fi
 
 cargo build --verbose --no-default-features --features=82x-rt --examples
 cargo build --verbose --no-default-features --features=845-rt --examples
