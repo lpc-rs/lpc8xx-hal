503f7e4cd78469843156e843723c40307b473f7c
Hanno Braun <hanno@braun-embedded.com>
2020-02-07 11:50:12 +0100

    Add RTFM example

---
+++ examples/rtfm.rs
@@ -0,0 +1,28 @@
+#![no_main]
+#![no_std]
+
+use lpc8xx_hal::{delay::Delay, prelude::*, Peripherals};
+use panic_halt as _;
+
+#[rtfm::app(device = lpc8xx_hal::pac)]
+const APP: () = {
+    #[init]
+    fn init(cx: init::Context) {
+        let p = Peripherals::take().unwrap();
+
+        let mut delay = Delay::new(cx.core.SYST);
+
+        let mut syscon = p.SYSCON.split();
+        let swm = p.SWM.split();
+        let gpio = p.GPIO.enable(&mut syscon.handle);
+
+        let mut led = swm.pins.pio1_1.into_gpio_pin(&gpio).into_output();
+
+        loop {
+            led.set_high().unwrap();
+            delay.delay_ms(700_u16);
+            led.set_low().unwrap();
+            delay.delay_ms(50_u16);
+        }
+    }
+};
