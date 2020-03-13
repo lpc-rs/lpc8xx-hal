6292bfee628cf439078b60d0d87715b7ab7bc499
Hanno Braun <hanno@braun-embedded.com>
2020-03-13 12:16:15 +0100

    Improve formatting
    With the added space and changed grouping, I find this much easier to
    read.

--- src/syscon/clocksource.rs
+++ src/syscon/clocksource.rs
@@ -1,10 +1,12 @@
 //! Clock configuration for the peripherals
+
 #[cfg(feature = "82x")]
 mod clocksource_82x;
-#[cfg(feature = "82x")]
-pub use clocksource_82x::*;
 #[cfg(feature = "845")]
 mod clocksource_845;
+
+#[cfg(feature = "82x")]
+pub use clocksource_82x::*;
 #[cfg(feature = "845")]
 pub use clocksource_845::*;
 
