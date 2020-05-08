80f4d3b2f3bb6ec4c632c2ec26e7c1dd41b4f93c
Hanno Braun <hanno@braun-embedded.com>
2020-05-08 14:35:26 +0200

    Reduce number of target selection `cfg`s
    This creates one module per target in `usart::clock` and moves all
    target-specific code there. I believe this is a bit cleaner, as only one
    `cfg` per target is required.
    
    The disadvantage are the redundant imports, but I believe it's a
    worthwhile trade-off. Especially since the imports required for the
    different targets are going to diverge going forward, which would
    require even more `cfg`s.

--- src/usart/clock.rs
+++ src/usart/clock.rs
@@ -1,11 +1,6 @@
 use core::marker::PhantomData;
 
-use crate::syscon::{
-    self,
-    clock_source::{PeripheralClock, PeripheralClockSource},
-};
-
-use super::instances::Instance;
+use crate::syscon::clock_source::PeripheralClockSource;
 
 /// Defines the clock configuration for a USART instance
 pub struct Clock<Clock> {
@@ -34,46 +29,69 @@ where
 }
 
 #[cfg(feature = "82x")]
-impl<I> PeripheralClock<I> for Clock<syscon::UARTFRG>
-where
-    I: Instance,
-{
-    fn select_clock(&self, _: &mut syscon::Handle) {
-        // NOOP, selected by default
+mod target {
+    use crate::{
+        syscon::{self, clock_source::PeripheralClock},
+        usart::Instance,
+    };
+
+    use super::Clock;
+
+    impl<I> PeripheralClock<I> for Clock<syscon::UARTFRG>
+    where
+        I: Instance,
+    {
+        fn select_clock(&self, _: &mut syscon::Handle) {
+            // NOOP, selected by default
+        }
     }
 }
 
 #[cfg(feature = "845")]
-impl Clock<syscon::IOSC> {
-    /// Create a new configuration with a specified baudrate
-    ///
-    /// Assumes the internal oscillator runs at 12 MHz
-    pub fn new_with_baudrate(baudrate: u32) -> Self {
-        // We want something with 5% tolerance
-        let calc = baudrate * 20;
-        let mut osrval = 5;
-        for i in (5..=16).rev() {
-            if calc * (i as u32) < 12_000_000 {
-                osrval = i;
+mod target {
+    use core::marker::PhantomData;
+
+    use crate::{
+        syscon::{
+            self,
+            clock_source::{PeripheralClock, PeripheralClockSource},
+        },
+        usart::Instance,
+    };
+
+    use super::Clock;
+
+    impl Clock<syscon::IOSC> {
+        /// Create a new configuration with a specified baudrate
+        ///
+        /// Assumes the internal oscillator runs at 12 MHz
+        pub fn new_with_baudrate(baudrate: u32) -> Self {
+            // We want something with 5% tolerance
+            let calc = baudrate * 20;
+            let mut osrval = 5;
+            for i in (5..=16).rev() {
+                if calc * (i as u32) < 12_000_000 {
+                    osrval = i;
+                }
+            }
+            let psc = (12_000_000 / (baudrate * osrval as u32) - 1) as u16;
+            let osrval = osrval - 1;
+            Self {
+                psc,
+                osrval,
+                _clock: PhantomData,
             }
         }
-        let psc = (12_000_000 / (baudrate * osrval as u32) - 1) as u16;
-        let osrval = osrval - 1;
-        Self {
-            psc,
-            osrval,
-            _clock: PhantomData,
-        }
     }
-}
 
-#[cfg(feature = "845")]
-impl<I, C> PeripheralClock<I> for Clock<C>
-where
-    I: Instance,
-    C: PeripheralClockSource,
-{
-    fn select_clock(&self, syscon: &mut syscon::Handle) {
-        syscon.fclksel[I::REGISTER_NUM].write(|w| w.sel().variant(C::CLOCK));
+    impl<I, C> PeripheralClock<I> for Clock<C>
+    where
+        I: Instance,
+        C: PeripheralClockSource,
+    {
+        fn select_clock(&self, syscon: &mut syscon::Handle) {
+            syscon.fclksel[I::REGISTER_NUM]
+                .write(|w| w.sel().variant(C::CLOCK));
+        }
     }
 }
