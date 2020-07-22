64518b24d79ef967a4ebfc8b5c5f41d7e0b63118
Hanno Braun <hanno@braun-embedded.com>
2020-07-22 14:36:40 +0200

    Add USART/DMA example
    Close #119

---
+++ examples/usart_dma.rs
@@ -0,0 +1,51 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_rtt_target;
+
+use lpc8xx_hal::{cortex_m_rt::entry, dma, usart, Peripherals};
+
+#[entry]
+fn main() -> ! {
+    rtt_target::rtt_init_print!();
+
+    let p = Peripherals::take().unwrap();
+
+    static mut DMA_DESCRIPTOR: dma::DescriptorTable =
+        dma::DescriptorTable::new();
+    // Sound, as this is the only place where we do this.
+    let dma_descriptors = unsafe { &mut DMA_DESCRIPTOR };
+
+    let swm = p.SWM.split();
+    let dma = p.DMA.split(dma_descriptors);
+    let mut syscon = p.SYSCON.split();
+
+    let dma_handle = dma.handle.enable(&mut syscon.handle);
+    let mut swm_handle = swm.handle.enable(&mut syscon.handle);
+
+    let clock_config = usart::Clock::new_with_baudrate(115200);
+
+    let (u0_rxd, _) = swm
+        .movable_functions
+        .u0_rxd
+        .assign(p.pins.pio0_24.into_swm_pin(), &mut swm_handle);
+    let (u0_txd, _) = swm
+        .movable_functions
+        .u0_txd
+        .assign(p.pins.pio0_25.into_swm_pin(), &mut swm_handle);
+
+    let serial =
+        p.USART0
+            .enable(&clock_config, &mut syscon.handle, u0_rxd, u0_txd);
+
+    let channel = dma.channels.channel1.enable(&dma_handle);
+
+    serial
+        .tx
+        .write_all(b"Hello, world!\r\n", channel)
+        .wait()
+        .expect("USART write shouldn't fail");
+
+    // We're done. Let's do nothing until someone resets the microcontroller.
+    loop {}
+}
