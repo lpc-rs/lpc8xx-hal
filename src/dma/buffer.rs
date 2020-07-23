d8ff36f25c564f540af54c978c458c035d146bd3
Hanno Braun <hanno@braun-embedded.com>
2020-07-23 15:17:07 +0200

    Support buffers as transfer destinations

--- src/dma/buffer.rs
+++ src/dma/buffer.rs
@@ -1,6 +1,9 @@
-use crate::pac::dma0::channel::xfercfg::SRCINC_A;
+use crate::{
+    pac::dma0::channel::xfercfg::{DSTINC_A, SRCINC_A},
+    void::Void,
+};
 
-use super::Source;
+use super::{Dest, Source};
 
 impl crate::private::Sealed for &'static [u8] {}
 
@@ -22,5 +25,35 @@ impl Source for &'static [u8] {
     fn end_addr(&self) -> *const u8 {
         // Sound, as we stay within the bounds of the slice.
         unsafe { self.as_ptr().add(self.len() - 1) }
+    }
+}
+
+impl crate::private::Sealed for &'static mut [u8] {}
+
+impl Dest for &'static mut [u8] {
+    /// The error that can occur while waiting for the destination to be idle
+    type Error = Void;
+
+    fn is_full(&self) -> bool {
+        self.len() == 0
+    }
+
+    fn increment(&self) -> DSTINC_A {
+        DSTINC_A::WIDTH_X_1
+    }
+
+    fn transfer_count(&self) -> Option<u16> {
+        // The cast should be fine, as DMA buffers are restricted to a length of
+        // 1024.
+        Some(self.len() as u16 - 1)
+    }
+
+    fn wait(&mut self) -> nb::Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn end_addr(&mut self) -> *mut u8 {
+        // Sound, as we stay within the bounds of the slice.
+        unsafe { self.as_mut_ptr().add(self.len() - 1) }
     }
 }
