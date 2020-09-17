4ea387c840657c8871c19f3b8307f1960364478a
Hanno Braun <hanno@braun-embedded.com>
2020-09-17 13:33:41 +0200

    Move check about rustfmt on CI to reusable var

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,11 +9,21 @@ set -e
 # `.cargo/config` for convenience, this variable is required.
 HOST_TARGET=${HOST_TARGET:-x86_64-unknown-linux-gnu}
 
+# Determine whether this build should run stable checks. This should be the case
+# on CI when the stable channel is used, or locally regardless of Rust version
+# used for the build.
+[ "$TRAVIS_RUST_VERSION" = stable ] && \
+    STABLE_CI_BUILD=true || STABLE_CI_BUILD=false
+[ -z "$TRAVIS_RUST_VERSION" ] && \
+    NO_CI_BUILD=true || NO_CI_BUILD=false
+([ "$STABLE_CI_BUILD" = true ] || [ "$NO_CI_BUILD" = true ]) && \
+    STABLE_CHECKS=true || STABLE_CHECKS=false
+
 # Fail build, if there are any warnings.
 export RUSTFLAGS="-D warnings"
 
 # Check for formatting with the stable rustfmt
-if [ "$TRAVIS_RUST_VERSION" != beta ] && [ "$TRAVIS_RUST_VERSION" != nightly ]; then
+if [ "$STABLE_CHECKS" = true ]; then
     # Only install rustup on stable, since it's not needed otherwise (and sometimes unavailable)
     rustup component add rustfmt
     cargo fmt -- --check
