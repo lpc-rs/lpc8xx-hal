5c6dc4c6d9afce50d06eebdd2025ac339a098a1c
Hanno Braun <hanno@braun-embedded.com>
2020-09-17 12:59:53 +0200

    Replace compiletest_rs with trybuild

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,25 +1,7 @@
-#![cfg(feature = "compiletest")]
-
-extern crate compiletest_rs as compiletest;
-
-use std::path::PathBuf;
+#![cfg(feature = "trybuild")]
 
 #[test]
 fn compile_test() {
-    run_mode("compile-fail");
-}
-
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
-
-    config.mode = mode.parse().expect("Failed to parse mode");
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
-
-    // Needed by the compiler to find other crates
-    config.link_deps();
-
-    // Fixes E0464, "multiple matching crates"
-    config.clean_rmeta();
-
-    compiletest::run_tests(&config);
+    let test_cases = trybuild::TestCases::new();
+    test_cases.compile_fail("tests/compile-fail/swm/*.rs");
 }
