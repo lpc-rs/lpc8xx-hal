6914a9e66f72006a7458b9690fb1da12c37b95c2
Hanno Braun <hanno@braun-embedded.com>
2021-01-05 16:50:07 +0100

    Add unit testing infrastructure
    Those two nested crates are a bit ugly, but I haven't found a way to
    condense it into a single crate, due to reasons I don't understand.

---
+++ test-suite/src/lib.rs
@@ -0,0 +1,17 @@
+#![no_std]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _; // panic handler
+
+// // Re-export dependencies, so test suite can use them.
+// pub extern crate defmt;
+// pub extern crate lpc8xx_hal;
+
+use lpc8xx_hal::cortex_m::asm;
+
+/// Causes probe-run to exit with exit code 0
+pub fn exit() -> ! {
+    loop {
+        asm::bkpt();
+    }
+}
